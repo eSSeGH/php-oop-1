ac20783e264f29426ea06c10c6e8007750b6b438
eSSeGH <ste.virgadaula@gmail.com>
2023-03-30 17:18:59 +0200

    BONUS2 accomplished

--- index.php
+++ index.php
@@ -1,30 +1,6 @@
-<?php
+<?php 
 
-require_once "./models/movie.php";
-
-$movie1 = new Movie(
-    'La vita è bella',
-    'it_IT',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, molestias. Nostrum beatae totam autem dolor, accusamus excepturi maiores ad reprehenderit?',
-    1997,
-    new MovieGenre('commedia'),
-    './img/locandina-lavitaèbella.jpg' );
-
-$movie1->film_genre->movie_genre2 = 'drammatico';
-
-$movie2 = new Movie(
-    'Tenet',
-    'eng_US',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, molestias. Nostrum beatae totam autem dolor, accusamus excepturi maiores ad reprehenderit?',
-    2020,
-    new MovieGenre('azione'),
-    './img/locandina-tenet.jpg' );
-
-$movie2->film_genre->movie_genre2 = 'fantascienza';
-$movie2->film_genre->movie_genre3 = 'spionaggio';
-$movie2->film_genre->movie_genre4 = 'thriller';
-
-var_dump($movie2->film_genre)
+require_once "./db.php";
 ?>
 
 <!DOCTYPE html>
